3489affa21190fec380ab7d008a001abb16aff0a
Luis <thecodingmenace@gmail.com>
2020-05-14 18:58:32 -0400

    new method

--- src/com/company/MegaBytesConverter.java
+++ src/com/company/MegaBytesConverter.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+    /*
+    This method should
+   //TODO calculate the megabytes and remaining kilobytes from the parameter passed
+  //TODO print a message in the format "XX KB = YY MB and ZZ KB"
+    XX = kiloBytes
+    YY = megabytes
+    ZZ = remaining kilobytes
+
+    ex: 2500 kilobytes prints "2500 KB = 2 MB and 452 KB"
+
+     */
+    }
 }
