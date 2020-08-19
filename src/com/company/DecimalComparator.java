1e812241b1d76cdc5044efd3118bf041096ebf73
CodingMenace <thecodingmenace@gmail.com>
2020-08-19 17:40:02 -0400

    new method again

--- src/com/company/DecimalComparator.java
+++ src/com/company/DecimalComparator.java
@@ -2,4 +2,13 @@ package com.company;
 
 public class DecimalComparator {
 
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b) {
+        a = a * 1000;
+        b = b * 1000;
+
+        int a1 = (int) a;
+        int b1 = (int) b;
+
+        return a1 == b1;
+    }
 }
