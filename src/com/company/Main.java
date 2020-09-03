24290782bc182dcddd72aaad6241af0ccfd358a9
CodingMenace <thecodingmenace@gmail.com>
2020-09-03 10:03:23 -0400

    challenge question

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-       long miles = SpeedConverter.toMilesPerHour(10.5);
-        System.out.println("Miles = " + miles);
+//       long miles = SpeedConverter.toMilesPerHour(10.5);
+//        System.out.println("Miles = " + miles);
+//        SpeedConverter.printConversion(10.5);
 
-        SpeedConverter.printConversion(10.5);
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New Score is " + newScore);
+
+        int unknownPlayer = calculateScore(350);
+        System.out.println("Unknown Score is " + unknownPlayer);
 
 
     }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore( int score){
+        System.out.println("Unknown Player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calcFeetAndInchesToCentimeters(int feet, int inches){
+
+        return -1;
+    }
+
+    public static int calcFeetAndInchesToCentimeters(int inches){
+
+        return -1;
+    }
+
+
 }
