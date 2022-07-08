d2a158c3238ed8be4d01788cce370a4a575b8f27
CodingMenace <thecodingmenace@gmail.com>
2022-07-08 18:36:39 -0400

    SecondAndMinutesChallenge Updated with shorter solution

--- src/com/company/SecondAndMinutesChallenge.java
+++ src/com/company/SecondAndMinutesChallenge.java
@@ -6,50 +6,88 @@ public class SecondAndMinutesChallenge {
 
 
         System.out.println(getDurationString(61, 0));
-        System.out.println(getDurationString(3600));
+        System.out.println(getDurationString(3945));
 
     }
 
-    public static String getDurationString(double minutes, double seconds) {
-
-        int hours = (int) minutes / 60;
-        int minutesLeftAfterHours = (int) minutes % 60;
-        int secondsToMinutes = (int) seconds / 60;
-        int secondsLeftAfterMinutes = (int) seconds % 60;
-
-        if (minutes < 0 || seconds < 0) {
+    private static String getDurationString(long seconds) {
+        if(seconds < 0){
             return "Invalid Value";
-        } else if (seconds >= 3600) {
-            hours = hours + ((int) seconds / 3600);
-            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
-        } else if (seconds >= 60) {
-            minutesLeftAfterHours = minutesLeftAfterHours + secondsToMinutes;
-            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
-        } else if ((minutes >= 0)) {
-            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
         }
-        return "Invalid value";
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes,remainingSeconds);
     }
 
-    public static String getDurationString(double seconds) {
 
-        if (seconds >= 0) {
-
-            int secondsToMinutes = (int) seconds / 60;
-            int secondsLeftAfterMinutes = (int) seconds % 60;
-
-            if (secondsLeftAfterMinutes == 0 && secondsToMinutes <= 59) {
-                return seconds + " seconds was divided evenly resulting in " + secondsToMinutes + " minute(s) and " + secondsLeftAfterMinutes + " seconds remaining";
+    private static String getDurationString(long minutes, long seconds) {
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59) ){
+            return  "Invalid Value";
+        }
+            long hours = minutes / 60;
+            long remainingMinutes = minutes % 60;
+            String hoursString = hours + "h";
+            if (hours < 10){
+                hoursString = "0" + hoursString;
             }
-            if (secondsLeftAfterMinutes < 59 && secondsToMinutes <= 59) {
-
-                return secondsToMinutes + " minutes and " + secondsLeftAfterMinutes + " seconds";
+            String minutesString = remainingMinutes + "m";
+            if (remainingMinutes < 10){
+                minutesString = "0" + minutesString;
             }
-
-            return getDurationString(secondsToMinutes, secondsLeftAfterMinutes);
+        String secondsString = seconds + "s";
+        if (seconds< 10){
+            secondsString = "0" + secondsString;
         }
 
-        return "Invalid";
+
+            return hoursString + " " +  minutesString + " " +  secondsString ;
     }
+
+    
+    
+    
+//
+//    public static String getDurationString(double minutes, double seconds) {
+//
+//        int hours = (int) minutes / 60;
+//        int minutesLeftAfterHours = (int) minutes % 60;
+//        int secondsToMinutes = (int) seconds / 60;
+//        int secondsLeftAfterMinutes = (int) seconds % 60;
+//
+//        if (minutes < 0 || seconds < 59) {
+//            return "Invalid Value";
+//        } else if (seconds >= 3600) {
+//            hours = hours + ((int) seconds / 3600);
+//            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
+//        } else if (seconds >= 60) {
+//            minutesLeftAfterHours = minutesLeftAfterHours + secondsToMinutes;
+//            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
+//        } else if ((minutes >= 0)) {
+//            return hours + "h " + minutesLeftAfterHours + "m " + secondsLeftAfterMinutes + "s";
+//        }
+//        return "Invalid value";
+//    }
+//
+//    public static String getDurationString(double seconds) {
+//
+//        if (seconds >= 0) {
+//
+//            int secondsToMinutes = (int) seconds / 60;
+//            int secondsLeftAfterMinutes = (int) seconds % 60;
+//
+//            if (secondsLeftAfterMinutes == 0 && secondsToMinutes <= 59) {
+//                return seconds + " seconds was divided evenly resulting in " + secondsToMinutes + " minute(s) and " + secondsLeftAfterMinutes + " seconds remaining";
+//            }
+//            if (secondsLeftAfterMinutes < 59 && secondsToMinutes <= 59) {
+//
+//                return secondsToMinutes + " minutes and " + secondsLeftAfterMinutes + " seconds";
+//            }
+//
+//            return getDurationString(secondsToMinutes, secondsLeftAfterMinutes);
+//        }
+//
+//        return "Invalid";
+//    }
 
 }
