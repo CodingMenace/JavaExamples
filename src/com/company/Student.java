d2e5a8f909c34092b9145a0c90e27f79b77cb529
CodingMenace <thecodingmenace@gmail.com>
2022-07-12 17:04:17 -0400

    New Student.java Class

---
+++ src/com/company/Student.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Student {
+    String firstName;
+    String lastname;
+    String courseFocus;
+
+    //Software Objects store it's State in Fields
+    //" Behavior in Methods
+
+
+}
