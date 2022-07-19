125ad58c09021acf3f0685c854d6cd387c0567f3
CodingMenace <thecodingmenace@gmail.com>
2022-07-19 10:03:51 -0400

    WhileAndDoWhile added Greeter & Person.java added

--- src/com/company/WhileAndDoWhile.java
+++ src/com/company/WhileAndDoWhile.java
@@ -11,6 +11,7 @@ public class WhileAndDoWhile {
             System.out.println(count);
             count++;
         }
+
         for (count = 1; count != 6 ; count++) {
             System.out.println(count);
         }
