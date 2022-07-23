53e4c94877b55e4dc618a0dd3424c9164bfcb22b
CodingMenace <thecodingmenace@gmail.com>
2022-07-23 16:49:29 -0400

    Update ChallengeQuestions

--- src/com/company/ChallengeQuestions.java
+++ src/com/company/ChallengeQuestions.java
@@ -3,9 +3,15 @@ package com.company;
 public class ChallengeQuestions {
 
 
-    public static void printDayOfTheWeek ( int day){
+    public static void main(String[] args) {
 
-        switch (day){
+        System.out.println(DigitSum(355));
+    }
+
+
+    public static void printDayOfTheWeek(int day) {
+
+        switch (day) {
             case 1:
                 System.out.println("Sunday");
                 break;
@@ -31,25 +37,41 @@ public class ChallengeQuestions {
         }
     }
 
-    public static void printDayOfTheWeekIfElse(int day){
+    public static void printDayOfTheWeekIfElse(int day) {
 
-        if (day == 1){
+        if (day == 1) {
             System.out.println("Sunday");
-        } else if (day == 2){
+        } else if (day == 2) {
             System.out.println("Monday");
-        } else if (day == 3){
+        } else if (day == 3) {
             System.out.println("Tuesday");
-        } else if (day == 4){
-        System.out.println("Wednesday");
-    } else if (day == 5){
+        } else if (day == 4) {
+            System.out.println("Wednesday");
+        } else if (day == 5) {
             System.out.println("Thursday");
-        } else if (day == 6){
+        } else if (day == 6) {
             System.out.println("Friday");
-        } else if (day == 7){
+        } else if (day == 7) {
             System.out.println("Saturday");
         } else
             System.out.println("Invalid Day");
 
+    }
+
+    public static int DigitSum(int num) {
+        int sum = 0;
+        int digit;
+        if (num >= 10) {
+
+            while (num > 0) {
+                digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+        }
+
+
+        return sum;
     }
 
 }
