ef15b704f4c5cc3116c1d7afd8d03d5988d06267
Luis Jimenez <thecodingmenace@gmail.com>
2022-09-14 01:49:01 -0400

    Added Break6 class

--- src/com/company/WhileAndDoWhile.java
+++ src/com/company/WhileAndDoWhile.java
@@ -151,5 +151,25 @@ class Break5{
 }
 //Where you put a label is important
 class Break6 {
-    
+    public static void main(String[] args) {
+        int x, y;
+
+        // here label before statement
+
+        stop1:
+        for (x = 0; x < 5; x++) {
+            for (y = 0; y < 5; y++) {
+                if (y == 2) break stop1;
+                System.out.println("x and y" + x + " " + y);
+            }
+        }
+        System.out.println();
+        //now label before {
+        for (x = 0; x < 5; x++) stop2: {
+            for (y = 0; y < 5; y++) {
+                if (y == 2) break stop2;
+                System.out.println("x and y" + x + " " + y);
+            }
+        }
+    }
 }
